c2b72783f48a7b60932d07f0c5f266935ded64db
ItsRauf <raufislam2003@gmail.com>
2020-09-12 17:47:42 -0400

    i just want this to work

--- src/sendCommits.js
+++ src/sendCommits.js
@@ -3,6 +3,7 @@ const getLatestCommit = require("./getLatestCommit");
 const Commit = require("./models/Commit");
 const Server = require("./models/Server");
 const sendSingleComment = require("./sendSingleComment");
+const sendEmbed = require("./sendEmbed");
 
 /**
  * Parses build number from title
@@ -33,14 +34,20 @@ module.exports = async function sendCommits(DatamineBot) {
       const channel = s.channels.resolve(server.channel);
       getLatestCommit().then(async (commit) => {
         if (!server.lastSentComment) {
-          return await sendSingleComment(DatamineBot, commit);
+          await Server.findByIdAndUpdate(server._id, {
+            lastSentComment: commit._id,
+          });
+          return await sendEmbed(channel, commit, server.roleid);
         } else if (commit._id > server.lastSentComment) {
           const commits = await Commit.find({
             _id: { $gt: server.lastSentComment },
           }).sort("buildNumber");
 
           for (const commit of commits) {
-            await sendSingleComment(DatamineBot, commit);
+            await Server.findByIdAndUpdate(server._id, {
+              lastSentComment: commit._id,
+            });
+            await sendEmbed(channel, commit, server.roleid);
           }
         }
       });
