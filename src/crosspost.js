93a7b95dc427aebbe089a47612a25707cd49e5d3
ItsRauf <raufislam2003@gmail.com>
2020-09-25 18:29:31 -0400

    add back pings and crossposting

--- src/crosspost.js
+++ src/crosspost.js
@@ -4,11 +4,11 @@
  * @param {import("discord.js").Message} message
  */
 module.exports = async function crosspost(message) {
-  // if (message.channel.type === "news") {
-  //   message.client.api
-  //     .channels(message.channel.id)
-  //     .messages(message.id)
-  //     .crosspost()
-  //     .post();
-  // }
+  if (message.channel.type === "news") {
+    message.client.api
+      .channels(message.channel.id)
+      .messages(message.id)
+      .crosspost()
+      .post();
+  }
 };
