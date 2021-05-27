ba149ddc9104cd0904507c671d66d836e18226c6
ItsRauf <raufislam2003@gmail.com>
2021-05-26 22:04:57 -0400

    refactors and stuffs

--- src/sendEmbed.js
+++ src/sendEmbed.js
@@ -1,52 +1,41 @@
 const ConstructEmbed = require("./ConstructEmbed");
 
 /**
- * Sends Embed
- *
  * @param {import("discord.js").TextChannel} channel
  * @param {import("mongoose").Document} commit
+ * @param {string} roleid
  */
 module.exports = async function sendEmbed(channel, commit, roleid) {
   try {
     if (!channel) return;
-    await channel
-      .send(roleid ? `<@&${roleid}>` : "", ConstructEmbed(commit))
-      // .send(ConstructEmbed(commit))
-      .then(async (msg) => {
-        if (msg.crosspostable) {
-          await msg.crosspost();
+    if (!commit) return;
+
+    const commitMSG = await channel.send(
+      roleid ? `<@&${roleid}>` : "",
+      ConstructEmbed(commit)
+    );
+
+    if (commitMSG.crosspostable) {
+      await commitMSG.crosspost();
+    }
+
+    if (commit.images && commit.images.length > 0) {
+      if (commit.images.length <= 5) {
+        const imgMSG = await channel.send(commit.images.join("\n"));
+        if (imgMSG.crosspostable) {
+          await imgMSG.crosspost();
         }
-        if (commit.images && commit.images.length > 0) {
-          if (commit.images.length <= 5) {
-            await channel.send(commit.images.join("\n")).then(async (msg) => {
-              if (msg.crosspostable) {
-                await msg.crosspost();
-              }
-            });
-          } else {
-            const length = Math.ceil(commit.images.length / 5);
-            for (let index = 0; index < length; index++) {
-              await channel
-                .send(commit.images.splice(index, (index + 1) * 5).join("\n"))
-                .then(async (msg) => {
-                  if (msg.crosspostable) {
-                    await msg.crosspost();
-                  }
-                });
-            }
-          }
-        }
-      });
+      }
+    }
 
     if (commit.comments) {
-      commit.comments.forEach(async (comment) => {
+      for (comment of commit.comments) {
         await sendEmbed(channel, {
           title: commit.title,
           ...comment,
         });
-      });
+      }
     }
-    return Promise.resolve();
   } catch (error) {
     return Promise.reject(error);
   }
