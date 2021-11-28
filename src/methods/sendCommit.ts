c3ea8a712553b9cfd8d3df05e920a5f5db0f6a84
ItsRauf <raufislam2003@gmail.com>
2021-11-27 20:51:49 -0500

    try catch pt 2

--- src/methods/sendCommit.ts
+++ src/methods/sendCommit.ts
@@ -10,42 +10,85 @@ export default async function sendCommit(
   server: Server,
   force?: boolean
 ): Promise<void> {
-  const guild = $.guilds.cache.get(server._id);
-  if (guild) {
-    const channel = guild.channels.cache.get(server.channel) as TextChannel;
-    if (channel) {
-      if (force || (server.lastSentComment ?? 0) < commit._id) {
-        const commitMSG = await channel.send({
-          content: server.role ? `<@&${server.role}>` : null,
-          embeds: [constructEmbed(commit)],
-        });
-        if (!force) {
-          await Server.updateOne(
-            {
-              _id: server._id,
-            },
-            { lastSentComment: commit._id }
-          );
+  try {
+    const guild = await $.guilds.fetch(server._id);
+    if (guild) {
+      try {
+        const channel = (await guild.channels.fetch(
+          server.channel
+        )) as TextChannel;
+        if (channel) {
+          try {
+            if (force || (server.lastSentComment ?? 0) < commit._id) {
+              const commitMSG = await channel.send({
+                content: server.role ? `<@&${server.role}>` : null,
+                embeds: [constructEmbed(commit)],
+              });
+              if (!force) {
+                await Server.updateOne(
+                  {
+                    _id: server._id,
+                  },
+                  { lastSentComment: commit._id }
+                );
+              }
+              if (commitMSG.crosspostable) {
+                try {
+                  await commitMSG.crosspost();
+                } catch (error) {
+                  console.error(
+                    `${guild.name} // ${channel.name} errored on crosspost`
+                  );
+                }
+              }
+              if (commit.images) {
+                const imgChunks = Array.from(
+                  { length: Math.ceil(commit.images.length / 5) },
+                  (v, i) => commit.images.slice(i * 5, i * 5 + 5)
+                );
+                try {
+                  const imgMsgs = await Promise.all(
+                    imgChunks.map(chunk => channel.send(chunk.join('\n')))
+                  );
+                  await Promise.all(
+                    imgMsgs.map(msg => {
+                      try {
+                        return msg.crosspost();
+                      } catch (error) {
+                        return console.error(
+                          `${guild.name} // ${channel.name} errored on image crosspost`
+                        );
+                      }
+                    })
+                  );
+                } catch (error) {
+                  console.error(
+                    `${guild.name} // ${channel.name} errored on image sending`
+                  );
+                }
+              }
+              if (commit.comments) {
+                await Promise.all(
+                  commit.comments.map(comment => sendCommit($, comment, server))
+                );
+              }
+            }
+          } catch (error) {
+            console.error(`${guild.name} // ${channel.name} errored`);
+          }
         }
-        if (commitMSG.crosspostable) {
-          await commitMSG.crosspost();
-        }
-        if (commit.images) {
-          const imgChunks = Array.from(
-            { length: Math.ceil(commit.images.length / 5) },
-            (v, i) => commit.images.slice(i * 5, i * 5 + 5)
-          );
-          const imgMsgs = await Promise.all(
-            imgChunks.map(chunk => channel.send(chunk.join('\n')))
-          );
-          await Promise.all(imgMsgs.map(msg => msg.crosspost()));
-        }
-        if (commit.comments) {
-          await Promise.all(
-            commit.comments.map(comment => sendCommit($, comment, server))
-          );
-        }
+      } catch (error) {
+        console.log(
+          `${guild.name} is misconfigured. try to contact them if possible`
+        );
       }
+    }
+  } catch (error) {
+    console.log(`Deleting ${server._id} as it errored.`);
+    try {
+      await Server.deleteOne({ _id: server._id });
+    } catch (error) {
+      console.error(error);
     }
   }
 }
